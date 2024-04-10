8d09116f16b98b2aca0bad759f85e3b856834215
Zayar-Win <zayarwin7751@gmail.com>
2024-04-10 14:01:07 +0630

    done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->unsignedBigInteger('role_id')->default(1);
             $table->string('profile_picture')->nullable();
             $table->unsignedBigInteger('group_id');
